a83c41a18c07d74f00104040a27e7c64e16aace4
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-31 11:57:48 +0900

    violet: parts: Introduce raise to wake gesture
    * Highly inspired by iOS raise to wake
    * Add min interval for raise2wake
    * Check proximity before wakeup
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>

--- parts/src/org/lineageos/settings/doze/DozeService.java
+++ parts/src/org/lineageos/settings/doze/DozeService.java
@@ -68,9 +68,9 @@ public class DozeService extends Service {
     }
 
     private void onDisplayOn() {
-        if (DEBUG)
-            Log.d(TAG, "Display on");
-        if (DozeUtils.isPickUpEnabled(this)) {
+        if (DEBUG) Log.d(TAG, "Display on");
+        if (DozeUtils.isPickUpEnabled(this) ||
+                DozeUtils.isRaiseToWakeEnabled(this)) {
             mPickupSensor.disable();
         }
         if (DozeUtils.isHandwaveGestureEnabled(this) || DozeUtils.isPocketGestureEnabled(this)) {
@@ -79,9 +79,9 @@ public class DozeService extends Service {
     }
 
     private void onDisplayOff() {
-        if (DEBUG)
-            Log.d(TAG, "Display off");
-        if (DozeUtils.isPickUpEnabled(this)) {
+        if (DEBUG) Log.d(TAG, "Display off");
+        if (DozeUtils.isPickUpEnabled(this) ||
+                DozeUtils.isRaiseToWakeEnabled(this)) {
             mPickupSensor.enable();
         }
         if (DozeUtils.isHandwaveGestureEnabled(this) || DozeUtils.isPocketGestureEnabled(this)) {
