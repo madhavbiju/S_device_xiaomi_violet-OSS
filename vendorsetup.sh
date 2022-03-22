5178e32dd034cd8e799b2beb69e76351cccaccde
Madhav Biju <65170463+madhavbiju@users.noreply.github.com>
2022-03-22 10:56:16 +0530

    violet: Include vendorsetup.sh
    - Track Vendor
    - Track Kernel

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Vendor
+git clone https://github.com/madhavbiju/vendor_xiaomi_violet_oss.git vendor/xiaomi/violet
+
+# Kernel
+git clone https://github.com/raghavt20/kernel_sm6150.git kernel/xiaomi/violet
